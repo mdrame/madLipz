e92c09216067b597bb14aa88be1449cb6633f2a5
Mo <mohammeddrame@ymail.com>
2019-08-29 01:04:27 -0400

    first commit / logic done and work

---
+++ basicPython.py
@@ -0,0 +1,26 @@
+print("\n")
+
+player_name = raw_input("     Welcome to MadLipz: Please enter your name: ")
+# todo: after player enter name, create UIHube loading.
+
+print("                       Hit 1 for instruction and 2 to skipe \n \n ")
+
+
+
+
+november_of = raw_input("It was a  ______ , cold November day.: ")
+cooking = raw_input("I woke up to the ______ smell: ")
+bird = raw_input("of ______ : ")
+kithen = raw_input("roasting in the ______  downstairs.: ")
+ran = raw_input("I ______ down the stairs: ")
+help_make = raw_input("to see if I could help ______the dinner.: ")
+uncle_name = raw_input(""""My mom said, "See if _____ needs: """)
+bird_part = raw_input("a fresh ______.: ")
+h20 = raw_input("So I carried a tray of glasses full of ______: ")
+room_location = raw_input("into the ______ room.: ")
+print(" When I got there,: ")
+body_part = raw_input("I could'nt belive my ______!: ")
+print("There where ______ ______ on the ______!: ")
+object = raw_input()
+object_action = raw_input()
+object_location = raw_input()

0da3ff0fc72d153c872427acdf07b0c7d53b2473
mohammed <mdrame113@gmail.com>
2019-09-02 16:33:28 -0400

    printed story, and string concatination

--- madLipz.py
+++ madLipz.py
@@ -1,7 +1,8 @@
 from random import shuffle
 
-adjectives = list("blue") #explicite
+adjectives = list() #explicite
 nouns = list()
+
 
 #colors list
 colors = ['\033[95m', '\033[94m', '\033[92m','\033[93m','\033[91m' ]
@@ -23,16 +24,19 @@ def inputs():
     adjectives.append(input("Ajective: "))
     nouns.append(input("Noun: "))
 
+    print(nouns)
+    print(adjectives)
+
 def paragraph():
 
 
-        print(f"""{colors[0]}My name is Mohammed. skfhsss
-        fsfsffs
-        sr
-        fsrfgwrgjngksrhgwg
-        wgwrgwkguwruhwrgw
-        gwgwrguwhgwrgwrg
-        wgwg""")
+    print(f"{colors[0]} {nouns[0]} is the")
+    print(f"{adjectives[0]} teacher in the world.")
+    print(f"{nouns[1]} is {adjectives[1]} and")
+    print(f"{adjectives[2]} {nouns[2]} makes:")
+    print(f"learning {adjectives[3]}, especially in")
+    print(f"{nouns[3]}. school is {adjectives[4]},")
+    print(f"because of {nouns[4]}! ")
 
 
 

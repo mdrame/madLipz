dc428dfe51ca9cb8c3c622684e806e54b4d6393b
mohammed <mdrame113@gmail.com>
2019-09-02 06:03:02 -0400

    color added

--- madLipz.py
+++ madLipz.py
@@ -1,7 +1,14 @@
+from random import shuffle
 
-
-adjectives = list() #explicite
+adjectives = list("blue") #explicite
 nouns = list()
+
+#colors list
+colors = ['\033[95m', '\033[94m', '\033[92m','\033[93m','\033[91m','\033[0m' ]
+shuffle(colors) #randomze list here
+#print(colors) test
+
+
 
 def inputs():
 
@@ -16,5 +23,19 @@ def inputs():
     adjectives.append(input("Ajective: "))
     nouns.append(input("Noun: "))
 
+def paragraph():
+
+
+        print(f"""{colors[0]}My name is Mohammed. skfhsss
+        fsfsffs
+        sr
+        fsrfgwrgjngksrhgwg
+        wgwrgwkguwruhwrgw
+        gwgwrguwhgwrgwrg
+        wgwg""")
+
+
+
+
 inputs()
-print(adjectives, nouns)
+paragraph()
